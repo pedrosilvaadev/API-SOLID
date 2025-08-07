dc0f3008506aff3c98c7d54c38f5109f7bca2f2a
Pedro Silva <pedro_rike@hotmail.com.br>
2025-08-06 22:34:33 -0300

    feat: add get user profile use case

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,18 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from '@/repositories/users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    const user = prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { hash } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let sut: GetUserProfileUseCase
+let userRepository: InMemoryUsersRepository
+
+describe('Get User Profile use case', () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(userRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+
+    const createdUser = await userRepository.create({
+      name: 'John Doe',
+      email: '9z6wM@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('John Doe')
+    expect(user.email).toEqual('9z6wM@example.com')
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+
+    await expect(() => sut.execute({
+      userId: 'non-existing-id',
+    })).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+})
