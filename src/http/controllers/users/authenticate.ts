96a51c915b6682322b2d3c8bb1623ad243c7f943
Pedro Silva <pedro_rike@hotmail.com.br>
2025-08-08 23:50:10 -0300

    feat: add refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -21,7 +21,21 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
       }
     })
 
-    return reply.status(200).send({ token })
+    const refreshToken = await reply.jwtSign({}, {
+      sign: {
+        sub: user.id,
+        expiresIn: '7d'
+      }
+    })
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true
+      })
+      .status(200).send({ token })
   } catch (error) {
     if (error instanceof InvalidaCredentialsError) {
       return reply.status(400).send({ message: error.message })
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,13 @@ import { authenticate } from './authenticate'
 import { register } from './register'
 import { profile } from './profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // Authenticated
   app.get('/me', { onRequest: [verifyJwt] }, profile)
