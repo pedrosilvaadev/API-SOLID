815d93588d0d973d2716e74f363ed4d9ae9019af
Pedro Silva <pedro_rike@hotmail.com.br>
2025-08-11 21:46:58 -0300

    fix: memory gym import decimal

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,7 +1,7 @@
 import { Gym, Prisma } from '@prisma/client'
 import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
 import { randomUUID } from 'crypto'
-import { Decimal } from 'generated/prisma/runtime/library'
+import { Decimal } from '@prisma/client/runtime/library'
 import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 
 export class InMemoryGymsRepository implements GymsRepository {
